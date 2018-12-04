97a91d408a268bc1aaa093b7ac22b682f992781e
Eric Steen <ericsteen1@gmail.com>
2018-12-04 13:50:40 -0800

    String

---
+++ python_algorithms/strings/pattern_matching.py
@@ -0,0 +1,14 @@
+class ClassName(object):
+    """docstring for ."""
+    def __init__(self, arg):
+        super(, self).__init__()
+        self.arg = arg
+
+      # naive string pattern matching
+      def naive:
+
+      # knuth-morris-pratt algorithm for pattern matching strings
+      def knp(pat, str):
+
+      # rubin-karp string pattern matching
+      def rubinKarp(pat, str):
