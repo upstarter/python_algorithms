0f808705bf762496c07ceac571e69f5d223829f5
Eric Steen <ericsteen1@gmail.com>
2019-05-10 18:57:28 -0700

    comprehnesions, decorators

--- python_algorithms/collections/dictionaries/dict.py
+++ python_algorithms/collections/dictionaries/dict.py
@@ -1,3 +1,54 @@
+# Construct a dictionary from pairs
+
+# izip returns iterables
+# xrange is better than range
+# Loop over dict keys and values
+
+# creates many items in mem
+for k, v in d.items()
+
+# better in space
+for k, v in d.iteritems()
+
+names = []
+colors = []
+# faster than zip
+d = dict(izip(names, colors))
+
+# counting with dicts
+# ok:
+d = {}
+for color in colors
+  if color not in d:
+      d[color] = 0
+  d[color] += 1
+
+ # best:
+for color in colors:
+    d[color] = d.get(color, 0) + 1
+
+# grouping with dicts
+# ok:
+names = ['fred', 'jim']
+d = {}
+for name in names:
+    key = len(name)
+    if key not in d:
+        d[key] = []
+    d[key].append(name)
+
+# better:
+d = {}
+for name in names:
+    key = len(name)
+    d.setdefault(key, []).append(name)
+
+# most best:
+d = defaultdict(list)
+for name in names:
+    key = len(name)
+    d[key].append(name)
+
 # to convert a list of tuples to a dictionary using the first value as the key.
 # Looked like this (making a database of comic book files):
 
