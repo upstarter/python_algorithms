385ff56ff82d7adb48e5321dbc0eb0a9b7ee3c70
Eric Steen <ericsteen1@gmail.com>
2020-05-02 18:55:19 -0700

    MDP

--- python_algorithms/algorithms/ml/unsupervised/reinforcement_learning/mdp.py
+++ python_algorithms/algorithms/ml/unsupervised/reinforcement_learning/mdp.py
@@ -14,3 +14,53 @@ print(vi.policy)  # result is (0, 0, 0)
 # mdptoolbox?<ENTER>
 # mdptoolbox.mdp?<ENTER>
 # mdptoolbox.mdp.ValueIteration?<ENTER>
+
+
+# MDP Objects Stanford class CS221
+import sys
+
+sys.setrecursionlimit(10000)
+## Transportation, walk number of blocks or take Tram
+class MDP(object):
+    def __init__(self, N):
+        # N = number of blocks
+        self.N = N
+
+    def startState(self):
+        return 1
+
+    def isEnd(self, state):
+        return state == self.N
+
+    def actions(self, state):
+        # return list of valid actions
+        result = []
+        if state + 1 <= self.N:
+            result.append("walk")
+        if state * 1 <= self.N:
+            result.append("tram")
+        return result
+
+    def succProbReward(self, state, action):
+        # return list of ( newState, prob, reward) triplets
+        # state = s, action = a, newState = s'
+        # prob = T(s, a, s'), reward = Reward(s, a, s')
+        result = []
+        if action == "walk":
+            result.append((state + 1, 1.0, -1.0))
+        if action == "tram":
+            result.append((state * 2, 0.5, -2.0))
+            result.append((state, 0.5, -2.0))
+        return result
+
+    def discount(self):
+        return 1.0
+
+    def states(self):
+        return range(1, self.N + 1)
+
+
+mdp = MDP(N=10)
+print(mdp.actions(3))
+print(mdp.succProbReward(3, "walk"))
+print(mdp.succProbReward(3, "tram"))
